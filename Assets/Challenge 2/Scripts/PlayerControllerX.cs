dc3c3cdab04c88ed5d258ae874ebaa9fa8a106f4
Wenyao Li <wenyao.li@woven-planet.global>
2023-05-31 15:25:08 +0900

    Initial commit.

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float timer = 10.0f;
+    public float timerRate =10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += timerRate * Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timer > 10)
+        {
+            timer = 0;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 3);
+        transform.localScale = Vector3.one * 1.8f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(0.2f, 1.0f, 0.8f, 0.2f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(12.0f * Time.deltaTime, 5.0f, 5.0f);
+    }
+}
